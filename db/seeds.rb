12ae98b2f449979101812fe034b6e4c58aac91e9
Jahaira <jahaira.marrero@gmail.com>
2021-03-26 00:41:22 -0400

    Started seeds. Didn't finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Diets
+d1 = Diet.create(name: "Vegetarian", description: "Vegetarian meals do not contain fish, meat, or chicken.", image: "https://media.istockphoto.com/photos/bowl-dish-with-brown-rice-cucumber-tomato-green-peas-red-cabbage-picture-id1047798504?k=6&m=1047798504&s=612x612&w=0&h=SqHh8UZas82kGOifausDLjh5WcJRT4T4pFtBNB-BVg4=")
+
+#User
+u1 = User.create(name: "Milo", password: "Milo123", image:"https://scontent-lga3-2.xx.fbcdn.net/v/t1.0-9/104209251_10104193109362184_4188311370884100240_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=UTzEfZSepWEAX8i7XWP&_nc_ht=scontent-lga3-2.xx&oh=6bb762579306971815ff992d950b4780&oe=60818F9E")
+
+
+puts "Let's get some recipes 🔥🔥🔥🔥🔥!"
