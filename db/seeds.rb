2a9fcc4b4fd5e9c23fc81b1ab981c0c879c13b6c
Jahaira <jahaira.marrero@gmail.com>
2021-03-26 17:13:40 -0400

    no major changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 d1 = Diet.create(name: "Vegetarian", description: "Vegetarian meals do not contain fish, meat, or chicken.", image: "https://media.istockphoto.com/photos/bowl-dish-with-brown-rice-cucumber-tomato-green-peas-red-cabbage-picture-id1047798504?k=6&m=1047798504&s=612x612&w=0&h=SqHh8UZas82kGOifausDLjh5WcJRT4T4pFtBNB-BVg4=")
 
 #User
-u1 = User.create(name: "Milo", password: "Milo123", image:"https://scontent-lga3-2.xx.fbcdn.net/v/t1.0-9/104209251_10104193109362184_4188311370884100240_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=UTzEfZSepWEAX8i7XWP&_nc_ht=scontent-lga3-2.xx&oh=6bb762579306971815ff992d950b4780&oe=60818F9E")
+u1 = User.create(name: "Scout", password: "Scout123", image: "https://i.pinimg.com/474x/8b/c0/95/8bc0959b121dac853564de8856ebf999.jpg")
 
 
 # Recipe
